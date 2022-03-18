a74e980ee900a9e1109aadbec2519d98e06b8167
Kenjie Mallari <83747226+kojmallari@users.noreply.github.com>
2022-03-18 16:12:57 +0800

    Add files via upload

--- test.component.ts
+++ test.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent{
-  showData($event:any){
-    console.log("Button is clicked!"); if($event) {
-      console.log($event.target);
-      console.log($event.target.value);
-    }
+export class TestComponent implements OnInit {
+  presentDate = new Date();
+  price : number = 20000;
+  Fruits = ["Apple", "Bananas", "Oranges", "Grapes", "Mango", "Kiwi"];
+
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+
+  constructor() { }
+
+  ngOnInit(): void {
   }
+
 }
-
-
